3af9d438eb807d026c0014255d19b9136c546fe8
Shweta Shukla <shweta.shukla@Eternus.local>
2019-05-13 18:51:53 +0530

    -implemented beneficiaries and volunteer api

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const auth = require("./routes/auth");
 const accounts = require("./routes/accounts");
 const prospects = require("./routes/prospects");
 const donors = require("./routes/donors");
+const beneficiaries = require("./routes/beneficiaries");
+const volunteers = require("./routes/volunteers");
 
 module.exports = function(app) {
   app.use(express.json());
@@ -14,4 +16,7 @@ module.exports = function(app) {
   app.use("/leads", accounts);
   app.use("/prospects", accounts);
   app.use("/donors", donors);
+    app.use("/beneficiaries", beneficiaries);
+    app.use("/volunteers", volunteers);
 };
+
